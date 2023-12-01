0579de0e835b817d829a84fbc564d0ff6603c9d0
Benjamin Segall <bentekkie@code-tunnel.bentekkielocal.com>
2023-12-01 13:35:55 +0000

    Day 1

---
+++ day1/part1.py
@@ -0,0 +1,22 @@
+import pathlib
+import os
+
+THIS_DIR = pathlib.Path(__file__).parent.resolve()
+
+def first_num(line: str):
+    for c in line:
+        if c.isdigit():
+            return c
+
+def last_num(line: str):
+    for c in line[::-1]:
+        if c.isdigit():
+            return c
+s = 0
+with open(os.path.join(THIS_DIR, "input.txt")) as f:
+    for line in f.readlines():
+        num = int(first_num(line)+last_num(line))
+        #print(num)
+        s += num
+
+print(s)
