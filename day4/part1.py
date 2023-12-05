2db6fa442ebde2838e9fdc9f2e91c57f84bcc43b
Ben Segall <bentekkie@gmail.com>
2023-12-05 00:16:19 +0000

    Format day 4

--- day4/part1.py
+++ day4/part1.py
@@ -7,28 +7,28 @@ THIS_DIR = pathlib.Path(__file__).parent.resolve()
 
 @dataclass
 class Card:
-    id_num : int
-    nums : list[int]
-    winning_nums : list[int]
+    id_num: int
+    nums: set[int]
+    winning_nums: set[int]
 
     def matches(self):
-        return len(set(self.nums).intersection(set(self.winning_nums)))
+        return len(self.nums.intersection(self.winning_nums))
 
 
-def parse_card(line : str):
+def parse_card(line: str):
     line = line.strip()
     line = line.removeprefix("Card ")
     id_part, rest = line.split(":")
     winning_part, nums_part = rest.split("|")
-    winning_nums = [int(s.strip()) for s in winning_part.split()]
-    nums = [int(s.strip()) for s in nums_part.split()]
+    winning_nums = {int(s.strip()) for s in winning_part.split()}
+    nums = {int(s.strip()) for s in nums_part.split()}
     return Card(int(id_part), nums, winning_nums)
+
 
 s = 0
 with open(os.path.join(THIS_DIR, "input.txt")) as f:
-        for line in f.readlines():
-             m = parse_card(line).matches()
-             if m > 0:
-                  s += 2**(m-1)
+    for line in f.readlines():
+        m = parse_card(line).matches()
+        s += (m > 0) * (2 ** (m - 1))
 
 print(s)
--- day4/part2.py
+++ day4/part2.py
@@ -1,39 +1,38 @@
 import pathlib
 import os
 from dataclasses import dataclass
-from functools import cached_property
 from collections import defaultdict
+
 THIS_DIR = pathlib.Path(__file__).parent.resolve()
 
 
 @dataclass
 class Card:
-    id_num : int
-    nums : list[int]
-    winning_nums : list[int]
-    count: int = 0
+    id_num: int
+    nums: set[int]
+    winning_nums: set[int]
 
-    @cached_property
     def matches(self):
-        return len(set(self.nums).intersection(set(self.winning_nums)))
+        return len(self.nums.intersection(self.winning_nums))
 
 
-def parse_card(line : str):
+def parse_card(line: str):
     line = line.strip()
     line = line.removeprefix("Card ")
     id_part, rest = line.split(":")
     winning_part, nums_part = rest.split("|")
-    winning_nums = [int(s.strip()) for s in winning_part.split()]
-    nums = [int(s.strip()) for s in nums_part.split()]
+    winning_nums = {int(s.strip()) for s in winning_part.split()}
+    nums = {int(s.strip()) for s in nums_part.split()}
     return Card(int(id_part), nums, winning_nums)
+
 
 instances = defaultdict(int)
 s = 0
 with open(os.path.join(THIS_DIR, "input.txt")) as f:
-        for line in f.readlines():
-             c = parse_card(line)
-             instances[c.id_num] += 1
-             s += instances[c.id_num]
-             for i in range(c.id_num+1, c.id_num+c.matches+1):
-                instances[i] += instances[c.id_num]
+    for line in f.readlines():
+        c = parse_card(line)
+        instances[c.id_num] += 1
+        s += instances[c.id_num]
+        for i in range(c.matches()):
+            instances[c.id_num + 1 + i] += instances[c.id_num]
 print(s)
