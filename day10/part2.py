74dcbf4a5ffff3795f7b61c277f73c4c37aa5171
Ben Segall <bentekkie@gmail.com>
2023-12-10 18:42:37 +0000

    reduce comparisons

--- day10/part2.py
+++ day10/part2.py
@@ -28,76 +28,67 @@ with open(os.path.join(THIS_DIR, "input.txt")) as f:
                 neighbors[(row, col + 1)].add(curr)
 
 
+sns = sorted(neighbors[start], key=lambda x: x[0])
+if sns[0][0] == sns[0][1]:
+    grid[start] = "-"
+elif sns[0][1] == sns[0][1]:
+    grid[start] = "|"
+elif sns[0][1] < start[1]:
+    grid[start] = "7"
+elif sns[0][1] > start[1]:
+    grid[start] = "F"
+elif sns[0][0] < start[0]:
+    grid[start] = "L"
+else:
+    grid[start] = "J"
+
+
 def far(start):
     seen = {start}
     to_check = neighbors[start]
     seen.update(to_check)
-    steps = 1
-    while True:
-        steps += 1
-        next_check = {
-            n
-            for p in to_check
-            for n in neighbors[p]
-            if n not in seen and p in neighbors[n]
-        }
-        seen.update(next_check)
-        if len(next_check) == 1:
-            return seen
-
-        to_check = next_check
+    while len(to_check) > 0:
+        seen.update(
+            to_check := {
+                n for p in to_check for n in (neighbors[p] - seen) if p in neighbors[n]
+            }
+        )
+    return seen
 
 
-path = far(start)
-
-start_c = ""
-sns = sorted(neighbors[start], key=lambda x: x[0])
-if sns[0][0] == sns[0][1]:
-    start_c = "-"
-elif sns[0][1] == sns[0][1]:
-    start_c = "|"
-elif sns[0][1] < start[1]:
-    start_c = "7"
-elif sns[0][1] > start[1]:
-    start_c = "F"
-elif sns[0][0] < start[0]:
-    start_c = "L"
-else:
-    start_c = "J"
+def is_out_of_bounds(p):
+    return p[0] <= 0 or p[1] <= 0 or p[0] >= rows or p[1] >= cols
 
 
 def grid_ns(p):
-    yield (p[0] - 0.5, p[1])
-    yield (p[0] + 0.5, p[1])
-    yield (p[0], p[1] - 0.5)
-    yield (p[0], p[1] + 0.5)
+    return {
+        (p[0] - 0.5, p[1]),
+        (p[0] + 0.5, p[1]),
+        (p[0], p[1] - 0.5),
+        (p[0], p[1] + 0.5),
+    }
 
 
 def filled(path: set[tuple[int, int]]):
     out = set()
     for p in path:
-        c = grid[p] if p != start else start_c
-        if c != ".":
+        if grid[p] != ".":
             out.add(p)
-        if c in "|LJ":
+        if grid[p] in "|LJ":
             out.add((p[0] - 0.5, p[1]))
-        if c in "|7F":
+        if grid[p] in "|7F":
             out.add((p[0] + 0.5, p[1]))
-        if c in "-J7":
+        if grid[p] in "-J7":
             out.add((p[0], p[1] - 0.5))
-        if c in "-LF":
+        if grid[p] in "-LF":
             out.add((p[0], p[1] + 0.5))
     return out
 
 
+path = far(start)
 filled_poses = filled(path)
 s = 0
-outside = set.union(
-    {(x, 0) for x in range(rows)},
-    {(x, cols) for x in range(rows)},
-    {(0, x) for x in range(cols)},
-    {(rows, x) for x in range(cols)},
-)
+outside = set()
 inside = set()
 for p in grid:
     if p not in path:
@@ -105,20 +96,13 @@ for p in grid:
         checked = set()
         is_outside = False
         while len(to_check) > 0 and not is_outside:
-            new_to_check = set()
-            for q in to_check:
-                checked.add(q)
-                if q in outside:
-                    is_outside = True
-                elif q in inside:
-                    break
-                else:
-                    new_to_check.update(
-                        n
-                        for n in grid_ns(q)
-                        if n not in filled_poses and n not in checked
-                    )
-            to_check = new_to_check
+            if len(to_check & outside) > 0 or any(map(is_out_of_bounds, to_check)):
+                is_outside = True
+                break
+            if len(to_check & inside) > 0:
+                break
+            checked.update(to_check)
+            to_check = set.union(*map(grid_ns, to_check)) - filled_poses - checked
         if is_outside:
             outside.update(checked)
         else:
