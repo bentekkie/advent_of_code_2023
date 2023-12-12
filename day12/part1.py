43cbba208b9bbdc1ffdaf8fcde0dfc1c4e50fd44
Benjamin Segall <bentekkie@gmail.com>
2023-12-12 05:01:03 -0500

    Update part1.py

--- day12/part1.py
+++ day12/part1.py
@@ -1,58 +1,40 @@
 import pathlib
 import os
+from functools import cache
 
 THIS_DIR = pathlib.Path(__file__).parent.resolve()
 
 
-def combs(line, sizes, curr=""):
+@cache
+def combs(line: str, sizes: tuple[int, ...]):
     if len(line) == 0:
-        if len(sizes) == 0:
-            yield ""
+        return len(sizes) == 0
     elif line[0] == "#":
-        if (
-            len(sizes) > 0
-            and len(line) >= sizes[0]
-            and all(c != "." for c in line[: sizes[0]])
-        ):
+        if len(sizes) > 0 and len(line) >= sizes[0] and not line[: sizes[0]].count("."):
             if len(line) > sizes[0] and line[sizes[0]] in ".?":
-                yield from (
-                    ("#" * sizes[0]) + "." + rest
-                    for rest in combs(
-                        line[sizes[0] + 1 :], sizes[1:], curr + ("#" * sizes[0]) + "."
-                    )
-                )
+                return combs(line[sizes[0] + 1 :], sizes[1:])
             elif len(line) == sizes[0] and len(sizes) == 1:
-                yield "#" * sizes[0]
+                return 1
+        return 0
     elif line[0] == "?":
         if len(sizes) == 0:
-            yield from ("." + rest for rest in combs(line[1:], sizes, curr + "."))
-        elif len(line) >= sizes[0] and all(c != "." for c in line[: sizes[0]]):
+            return combs(line[1:], sizes)
+        elif len(line) >= sizes[0] and not line[: sizes[0]].count("."):
             if len(line) > sizes[0]:
                 if line[sizes[0]] == "#":
-                    # print('.', line, sizes)
-                    yield from (
-                        "." + rest for rest in combs(line[1:], sizes, curr + ".")
-                    )
+                    return combs(line[1:], sizes)
                 else:
-                    # print('apply', line, sizes)
-                    yield from (
-                        ("#" * sizes[0]) + "." + rest
-                        for rest in combs(
-                            line[sizes[0] + 1 :],
-                            sizes[1:],
-                            curr + ("#" * sizes[0] + "."),
-                        )
-                    )
-                    yield from (
-                        "." + rest for rest in combs(line[1:], sizes, curr + ".")
+                    return combs(line[sizes[0] + 1 :], sizes[1:]) + combs(
+                        line[1:], sizes
                     )
             elif len(sizes) == 1:
-                yield "#" * sizes[0]
-                yield from ("." + rest for rest in combs(line[1:], sizes))
+                return 1 + combs(line[1:], sizes)
+            return 0
         elif len(line) > sizes[0]:
-            yield from ("." + rest for rest in combs(line[1:], sizes))
+            return combs(line[1:], sizes)
+        return 0
     else:
-        yield from (line[0] + rest for rest in combs(line[1:], sizes))
+        return combs(line[1:], sizes)
 
 
 s = 0
@@ -60,6 +42,6 @@ with open(os.path.join(THIS_DIR, "input.txt")) as f:
     for full_line in f.readlines():
         line, raw_sizes = full_line.strip().split(" ")
         sizes = [int(x) for x in raw_sizes.split(",")]
-        s += sum(1 for x in combs(line, sizes))
+        s += combs(line, sizes)
 
 print(s)
